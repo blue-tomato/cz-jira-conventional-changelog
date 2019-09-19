aeb222ca1a852ed184e5f95f5e720870979411f0
Roman <roman.lerchster@blue-tomato.com>
2019-09-19 14:48:35 +0200

    (NOTASK) feat: add questions

---
+++ index.js
@@ -0,0 +1,78 @@
+const types = require('conventional-commit-types');
+
+const getLongInput = ({ required = false, maxLength = 80 }) => ({
+  validate: value => {
+    const length = value.trim().length;
+
+    return required && length <= 0
+      ? 'required'
+      : length > maxLength
+      ? 'max ' + maxLength + ' chars'
+      : true;
+  },
+  transformer: value => '(' + value.trim().length + ') ' + value,
+  filter: value => value.trim()
+});
+
+module.exports = {
+  prompter: (cz, commit) => {
+    cz.prompt([
+      {
+        type: 'confirm',
+        name: 'isIssueAffected',
+        message: 'Does this change affect an issue?',
+        default: true
+      },
+      {
+        ...getLongInput({
+          required: true,
+          maxLength: 20
+        }),
+        type: 'input',
+        name: 'issue',
+        message: 'Issue reference:',
+        when: answers => answers.isIssueAffected,
+        default: undefined,
+        transformer: value =>
+          '(' + value.trim().length + ') ' + value.toUpperCase(),
+        filter: value => value.trim().toUpperCase()
+      },
+      {
+        type: 'list',
+        name: 'type',
+        message: "Select the type of change that you're committing:",
+        choices: Object.keys(types.types),
+        default: Object.keys(types.types)[0]
+      },
+      {
+        ...getLongInput({
+          required: false,
+          maxLength: 20
+        }),
+        type: 'input',
+        name: 'scope',
+        message: 'What is the scope of this change: (press enter to skip)',
+        default: undefined,
+        transformer: value =>
+          '(' + value.trim().length + ') ' + value.toLowerCase(),
+        filter: value => value.trim().toLowerCase()
+      },
+      {
+        ...getLongInput({
+          required: true,
+          maxLength: 60
+        }),
+        type: 'input',
+        name: 'subject',
+        message: 'Write a short, imperative tense description of the change:\n',
+        default: undefined
+      }
+    ]).then(({ issue, type, scope, subject }) =>
+      commit(
+        `${issue ? issue : '(NOTASK)'} ${type}${
+          scope.length >= 1 ? `(${scope})` : ''
+        }: ${subject}`
+      )
+    );
+  }
+};
